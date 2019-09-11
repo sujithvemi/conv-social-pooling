6d89f345bc32faaeabaa9f998f5a28c002e60610
sujithvemi <sujith.3920@gmail.com>
2019-09-11 09:30:20 +0530

    fixed an issue with indexing for trajectory

--- preprocess.py
+++ preprocess.py
@@ -2,27 +2,29 @@ import pandas as pd
 import numpy as np
 import multiprocessing as mp
 from scipy.io import savemat
+from tqdm import tqdm
 
-# us101_1 = '/home/sujith/Documents/Projects/ADS/prediction/datasets/NGSIM/us101/0750am-0805am/trajectories-0750am-0805am.txt'
-# us101_2 = '/home/sujith/Documents/Projects/ADS/prediction/datasets/NGSIM/us101/0805am-0820am/trajectories-0805am-0820am.txt'
-# us101_3 = '/home/sujith/Documents/Projects/ADS/prediction/datasets/NGSIM/us101/0820am-0835am/trajectories-0820am-0835am.txt'
-# i80_1 = '/home/sujith/Documents/Projects/ADS/prediction/datasets/NGSIM/i80/0400pm-0415pm/trajectories-0400-0415.txt'
-# i80_2 = '/home/sujith/Documents/Projects/ADS/prediction/datasets/NGSIM/i80/0500pm-0515pm/trajectories-0500-0515.txt'
-# i80_3 = '/home/sujith/Documents/Projects/ADS/prediction/datasets/NGSIM/i80/0515pm-0530pm/trajectories-0515-0530.txt'
+us101_1 = '/home/sujith/Documents/Projects/ADS/prediction/datasets/NGSIM/us101/0750am-0805am/trajectories-0750am-0805am.txt'
+us101_2 = '/home/sujith/Documents/Projects/ADS/prediction/datasets/NGSIM/us101/0805am-0820am/trajectories-0805am-0820am.txt'
+us101_3 = '/home/sujith/Documents/Projects/ADS/prediction/datasets/NGSIM/us101/0820am-0835am/trajectories-0820am-0835am.txt'
+i80_1 = '/home/sujith/Documents/Projects/ADS/prediction/datasets/NGSIM/i80/0400pm-0415pm/trajectories-0400-0415.txt'
+i80_2 = '/home/sujith/Documents/Projects/ADS/prediction/datasets/NGSIM/i80/0500pm-0515pm/trajectories-0500-0515.txt'
+i80_3 = '/home/sujith/Documents/Projects/ADS/prediction/datasets/NGSIM/i80/0515pm-0530pm/trajectories-0515-0530.txt'
 
-us101_1 = '/home/sujith/Documents/prediction/datasets/NGSIM/us101/i101_trajectories-0750am-0805am.txt'
-us101_2 = '/home/sujith/Documents/prediction/datasets/NGSIM/us101/i101_trajectories-0805am-0820am.txt'
-us101_3 = '/home/sujith/Documents/prediction/datasets/NGSIM/us101/i101_trajectories-0820am-0835am.txt'
-i80_1 = '/home/sujith/Documents/prediction/datasets/NGSIM/i80/i80_trajectories-0400-0415.txt'
-i80_2 = '/home/sujith/Documents/prediction/datasets/NGSIM/i80/i80_trajectories-0500-0515.txt'
-i80_3 = '/home/sujith/Documents/prediction/datasets/NGSIM/i80/i80_trajectories-0515-0530.txt'
+# us101_1 = '/home/sujith/Documents/prediction/datasets/NGSIM/us101/i101_trajectories-0750am-0805am.txt'
+# us101_2 = '/home/sujith/Documents/prediction/datasets/NGSIM/us101/i101_trajectories-0805am-0820am.txt'
+# us101_3 = '/home/sujith/Documents/prediction/datasets/NGSIM/us101/i101_trajectories-0820am-0835am.txt'
+# i80_1 = '/home/sujith/Documents/prediction/datasets/NGSIM/i80/i80_trajectories-0400-0415.txt'
+# i80_2 = '/home/sujith/Documents/prediction/datasets/NGSIM/i80/i80_trajectories-0500-0515.txt'
+# i80_3 = '/home/sujith/Documents/prediction/datasets/NGSIM/i80/i80_trajectories-0515-0530.txt'
 
 files = [us101_1, us101_2, us101_3, i80_1, i80_2, i80_3]
 
 cols = ['dsID', 'vehID', 'frameNum', 'local_x', 'local_y', 'lane_id']
 trajectories = []
 
-for i, file in enumerate(files):
+print('Reading the data from raw text files')
+for i, file in tqdm(enumerate(files)):
     df = pd.read_csv(file, header=None, delimiter=r'\s+')
     data_id = pd.DataFrame((i+1)*np.ones(df.shape[0], dtype=np.int32))
     df = pd.concat([data_id, df.iloc[:, [0, 1, 4, 5, 13]]], axis=1)
@@ -41,11 +43,11 @@ def parse_fields(traj):
         dsID = traj.iloc[k, 0]
         vehID = traj.iloc[k, 1]
         vehTraj = traj.loc[(traj.iloc[:, 0] == dsID) & (traj.iloc[:, 1] == vehID), :]
-        ind = vehTraj[vehTraj[2] == time].iloc[0,:].name
+        ind = vehTraj[vehTraj.iloc[:, 2] == time].iloc[0,:].name
         lane = traj.iloc[k, 5]
 
         #Find lateral maneuver
-        ub = min(vehTraj.shape[0], ind+40)
+        ub = min(vehTraj.shape[0]-1, ind+40)
         lb = max(0, ind-40)
         if vehTraj.iloc[ub, 5] > vehTraj.iloc[ind, 5] or vehTraj.iloc[ind, 5] > vehTraj.iloc[lb,5]:
             traj.loc[k, 'lat_man'] = 3
@@ -55,8 +57,8 @@ def parse_fields(traj):
             traj.loc[k, 'lat_man'] = 1
 
         #Find longitudinal maneuver
-        ub = min(vehTraj.shape[0], ind+50)
-        lb = max(1, ind-30)
+        ub = min(vehTraj.shape[0]-1, ind+50)
+        lb = max(0, ind-30)
         if ub == ind or lb == ind:
             traj.loc[k, 'long_man'] = 1
         else:
@@ -91,9 +93,10 @@ def parse_fields(traj):
                     grid.loc[k, str(gridInd)] = frameR.iloc[l, 1]
     return pd.concat([traj, grid], axis=1)
 
-
+print('Processing the dataframes in parallel with 6 workers')
 p = mp.Pool(6)
 trajectories = p.map(parse_fields, trajectories)
+p.close()
 
 trajAll = pd.concat([trajectories[i] for i in range(len(trajectories))], axis=0, ignore_index=True)
 
@@ -103,6 +106,7 @@ trajTr = pd.DataFrame(columns=trajAll.columns)
 trajVal = pd.DataFrame(columns=trajAll.columns)
 trajTest = pd.DataFrame(columns=trajAll.columns)
 
+print('Splitting into train, val and test datasets')
 for i in range(1, 7):
     ul1 = round(0.7*trajAll.loc[trajAll.iloc[:, 0] == i, 1].max())
     ul2 = round(0.8*trajAll.loc[trajAll.iloc[:, 0] == i, 1].max())
@@ -111,7 +115,7 @@ for i in range(1, 7):
     trajVal = pd.concat([trajVal, trajAll.loc[(trajAll.iloc[:, 0] == i) & (trajAll.iloc[:, 1] > ul1) & (trajAll.iloc[:, 1] <= ul2), :]], axis=0, ignore_index=True)
     trajTest = pd.concat([trajTr, trajAll.loc[(trajAll.iloc[:, 0] == i) & (trajAll.iloc[:, 1] > ul2), :]], axis=0, ignore_index=True)
 
-
+print('Creating tracks')
 for i in range(1, 7):
     trajSet = trajTr.loc[trajTr.iloc[:, 0] == i, :]
     carIds = list(trajSet.iloc[:, 1].unique())
@@ -125,6 +129,7 @@ for i in range(1, 7):
     carIds = list(trajSet.iloc[:, 1].unique())
     tracksTest = {(i, carIds[j]):trajSet.loc[trajSet.iloc[:, 1] == carIds[j], [2, 3, 4]].to_numpy() for j in carIds}
 
+print('Keeping only tracks with more than 30 frames')
 trajTr['keep'] = np.zeros(trajTr.shape[0], dtype=np.bool)
 trajTr.loc[:, 'keep'] = trajTr.apply(lambda x: 1 if (tracksTr[(x[0], x[1])][0, 30] <= x[2]) & (tracksTr[(x[0], x[1])][0, -1] > x[2]) else 0, axis=1)
 trajTr.loc[trajTr.loc[:, 'keep'] == 1, :].drop('keep', axis=1, inplace=True)
@@ -142,6 +147,7 @@ trainTraj['traj'] = trajVal.to_numpy()
 testTraj = {}
 testTraj['traj'] = trajTest.to_numpy()
 
+print('saving into mat files')
 savemat('trainTraj', trainTraj)
 savemat('testTraj', testTraj)
 savemat('valTraj', valTraj)
@@ -149,3 +155,5 @@ savemat('valTraj', valTraj)
 savemat('trainTracks', tracksTr)
 savemat('valTracks', tracksVal)
 savemat('testTracks', tracksTest)
+
+print('Done!')
